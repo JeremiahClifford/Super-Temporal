2e78f433e3d9b374e9cbd75c7f345af3465e0e96
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2024-02-01 19:14:42 -0500

    Server Test -Created a basic server folder and node express server file to begin laying the foundation for making the game online multiplayer
    Signed-off-by: JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>

---
+++ Server/src/server.ts
@@ -0,0 +1,29 @@
+//express server setup
+const express = require("express")
+const bodyParser = require('body-parser')
+const app = express()
+
+const fs = require('fs')
+
+//data from the json files
+let settings = require('../data/settings.json')
+
+//settings to make data parsing and connecting work
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
+
+app.get("/test", (request, response) => {
+    response.send("Test Answer")
+})
+
+//opens the server on the specified port
+app.listen(settings.Port, () => {
+    console.log(`Listen on the port ${settings.Port}`)
+})
