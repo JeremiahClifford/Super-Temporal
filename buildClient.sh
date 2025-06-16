237fa5e19d757e56ea137e1aeba073340a627400
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2025-06-16 15:48:24 -0400

    Removed Client Build Functionality
    Removed the build functionality from the client as it is no longer needed as the client will be hosted asa a website.

--- buildClient.sh
+++
@@ -1,6 +0,0 @@
-mkdir Builds
-cd Client
-tsc
-cd ..
-zip "Client_$1.zip" ./Client/dist/* ./Client/images/* ./Client/index.html ./Client/style.css ./Client/launch-linux.sh ./Client/launch-windows.bat
-mv "Client_$1.zip" ./Builds
