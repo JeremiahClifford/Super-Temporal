fdd6ad42c33ec5c7832ec086bf1528bee00be977
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2025-07-09 14:37:35 -0400

    Button Styling and Logging Improvements
    Added some styling to make buttons look better. Changed the server launch script to create a folder with the date and time and put the logs in there so I can keep logs from previous tests and know when they are from. Changed the git ignore to reflect this change in log location.

--- Server/launch.sh
+++ Server/launch.sh
@@ -1,4 +1,7 @@
 curl https://ipinfo.io/ip
 echo ""
 echo "Server Running. Outputting to log.txt"
-npx ts-node server.ts > log.txt
+cd logs
+mkdir "`date +"%d-%m-%Y_%H:%M:%S"`"
+cd ..
+npx ts-node server.ts > logs/"`date +"%d-%m-%Y_%H:%M:%S"`"/log.txt 2> logs/"`date +"%d-%m-%Y_%H:%M:%S"`"/err.txt
