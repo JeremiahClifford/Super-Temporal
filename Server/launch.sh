c8efffe1b6536598e289a62b9f9fb918141d8efe
JeremiahClifford <63067769+JeremiahClifford@users.noreply.github.com>
2025-07-09 22:04:42 -0400

    Small Logging Improvement
    Added functionality to the Server launch script to let the host add an identifier to the log folder in addition to the date and time for easier identification.

--- Server/launch.sh
+++ Server/launch.sh
@@ -2,6 +2,6 @@ curl https://ipinfo.io/ip
 echo ""
 echo "Server Running. Outputting to log.txt"
 cd logs
-mkdir "`date +"%d-%m-%Y_%H:%M:%S"`"
+mkdir "[$1]_`date +"%d-%m-%Y_%H:%M:%S"`"
 cd ..
-npx ts-node server.ts > logs/"`date +"%d-%m-%Y_%H:%M:%S"`"/log.txt 2> logs/"`date +"%d-%m-%Y_%H:%M:%S"`"/err.txt
+npx ts-node server.ts > logs/"[$1]_`date +"%d-%m-%Y_%H:%M:%S"`"/log.txt 2> logs/"[$1]_`date +"%d-%m-%Y_%H:%M:%S"`"/err.txt
